d32a2e406cddfdf1e504e3e959546e6846a36517
sayosaolrak <sayosaolrak@gmail.com>
2023-03-09 10:01:34 +0800

    Added checkout

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './Screen/HomeScreen.js';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import Navbar from 'react-bootstrap/Navbar';
+import LinkContainer from 'react-router-bootstrap/LinkContainer';
+import Container from 'react-bootstrap/Container';
+import ProductScreen from './Screen/ProductScreen.js';
+import Badge from 'react-bootstrap/Badge';
+import Nav from 'react-bootstrap/Nav';
+import { useContext } from 'react';
+import { Store } from './Store.js';
+import CartScreen from './Screen/CartScreen.js';
+import SigninScreen from './Screen/SigninScreen.js';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import ShippingAddressScreen from './Screen/ShippingAddressScreen.js';
+import SignupScreen from './Screen/SignupScreen.js';
+import PaymentMethodScreen from './Screen/PaymentMethodScreen.js';
+import PlaceOrderScreen from './Screen/PlaceOrderScreen.js';
+
+function App() {
+  const { state, dispatch: ctxDispatch } = useContext(Store);
+  const { cart, userInfo } = state;
+
+  const signoutHandler = () => {
+    ctxDispatch({ type: 'USER_SIGNOUT' });
+    localStorage.removeItem('userInfo');
+    localStorage.removeItem('shippingAddress');
+  };
+  return (
+    <BrowserRouter>
+      <div className="d-flex flex-column site-container">
+        <ToastContainer position="bottom-center" limit={1} />
+        <header>
+          <Navbar bg="dark" variant="dark">
+            <Container className="mt-3">
+              <LinkContainer to="/">
+                <Navbar.Brand>kdJeyEss</Navbar.Brand>
+              </LinkContainer>
+              <Nav className="me-auto">
+                <Link to="/cart" className="nav-link">
+                  Cart
+                  {cart.cartItems.length > 0 && (
+                    <Badge pill bg="danger">
+                      {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                    </Badge>
+                  )}
+                </Link>
+                {userInfo ? (
+                  <NavDropdown
+                    title={userInfo.name}
+                    id="collapsible-nav-dropdown"
+                  >
+                    <LinkContainer to="/profile">
+                      <NavDropdown.Item>User Profile</NavDropdown.Item>
+                    </LinkContainer>
+                    <LinkContainer to="/orderhistory">
+                      <NavDropdown.Item>Order History</NavDropdown.Item>
+                    </LinkContainer>
+                    <NavDropdown.Divider />
+                    <Link
+                      className="dropdown-item"
+                      to="#signout"
+                      onClick={signoutHandler}
+                    >
+                      Sign Out
+                    </Link>
+                  </NavDropdown>
+                ) : (
+                  <Link className="nav-link" to="/signin">
+                    Sign In
+                  </Link>
+                )}
+              </Nav>
+            </Container>
+          </Navbar>
+        </header>
+        <main>
+          <Container className="mt-3">
+            <Routes>
+              <Route path="/product/:slug" element={<ProductScreen />} />
+              <Route path="/cart" element={<CartScreen />} />
+              <Route path="/signin" element={<SigninScreen />} />
+              <Route path="/signup" element={<SignupScreen />} />
+              <Route path="/placeorder" element={<PlaceOrderScreen />} />
+              <Route path="/" element={<HomeScreen />} />
+              <Route path="/payment" element={<PaymentMethodScreen />}></Route>
+              <Route
+                path="/shipping"
+                element={<ShippingAddressScreen />}
+              ></Route>
+            </Routes>
+          </Container>
+        </main>
+        <footer className="text-center">All Rights Reserved</footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,58 @@
+const data = {
+  products: [
+    {
+      name: 'Nike Slim Shirt',
+      slug: 'nike-slim-shirt',
+      category: 'Shirts',
+      image:
+        'https://res.cloudinary.com/dgzasex9s/image/upload/v1677735778/Ecom-img/1_henx7j.jpg',
+      price: 120,
+      countInStock: 10,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality shirt',
+    },
+    {
+      name: 'Adidas Fit Shirt',
+      slug: 'adidas-fit-shirt',
+      category: 'Shirts',
+      image:
+        'https://res.cloudinary.com/dgzasex9s/image/upload/v1677735778/Ecom-img/2_sgqqy0.jpg',
+      price: 250,
+      countInStock: 20,
+      brand: 'Adidas',
+      rating: 4.0,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Nike Slim Pant',
+      slug: 'nike-slim-pant',
+      category: 'Pants',
+      image:
+        'https://res.cloudinary.com/dgzasex9s/image/upload/v1677735778/Ecom-img/4_ulkwik.jpg',
+      price: 25,
+      countInStock: 15,
+      brand: 'Nike',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+    {
+      name: 'Adidas Fit Pant',
+      slug: 'adidas-slim-pant',
+      category: 'pants',
+      image:
+        'https://res.cloudinary.com/dgzasex9s/image/upload/v1677735778/Ecom-img/3_ukpa3c.jpg',
+      price: 65,
+      countInStock: 5,
+      brand: 'Puma',
+      rating: 4.5,
+      numReviews: 10,
+      description: 'high quality product',
+    },
+  ],
+};
+
+export default data;
